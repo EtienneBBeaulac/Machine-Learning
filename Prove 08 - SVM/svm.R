db8c4c86bbd5308d19b84aa1f8aecbae9254e148
Exaudeus <etiennebeaulac@gmail.com>
2018-03-05 09:29:15 -0700

    svm

---
+++ Prove 08 - SVM/svm.R
@@ -0,0 +1,7 @@
+library(e1071)
+library(readr)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
+vowel_dat <- read_csv("https://content.byui.edu/items/6a934411-cba6-495f-8294-f53bf2c9828c/1/vowel.csv")
